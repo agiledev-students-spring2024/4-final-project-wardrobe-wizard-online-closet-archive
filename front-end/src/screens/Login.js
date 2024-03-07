bf87c098d20e6d6beac488cf97cb3d2dfa572d1e
Navid Chowdhury <NC1939@nyu.edu>
2024-03-07 10:01:19 -0500

    created login page inside app.js

---
+++ front-end/src/screens/Login.js
@@ -0,0 +1,10 @@
+
+function Login(){
+    return(
+        <div className='login'>
+            <div className='brandName'>
+                <h1>Wardrobe Wizard</h1>
+            </div>
+        </div>
+    )
+}
