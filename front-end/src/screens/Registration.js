0dbcde7d7051dd1f41b5ff6acc15d9aaa08fccbd
Navid Chowdhury <NC1939@nyu.edu>
2024-03-07 11:19:47 -0500

    finished registration page and most of home page

---
+++ front-end/src/screens/Registration.js
@@ -0,0 +1,31 @@
+import '../styles/Login.css'
+import { useNavigate } from 'react-router-dom';
+
+function Registration(){
+    const navigate = useNavigate(); 
+    const handleSubmit  = () => {
+        navigate('/home')
+    }
+    return(
+        <div className="Login">
+            <div className='banner'>
+                <h1>WARDROBE WIZARD</h1>
+                <h3>Unlock Your Closet's Potential</h3>
+            </div>
+            <div className='login'>
+                <h2>Register</h2>
+                <form className='loginForm' onSubmit={handleSubmit}>
+                    <input name="username" placeholder='Enter Username' />
+                    <br/>
+                    <input name="password" placeholder='Enter Password' type ='password'/>
+                    <br/>
+                    <input name="password" placeholder='Confirm Password' type ='password' />
+                    <br/>
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Registration;
