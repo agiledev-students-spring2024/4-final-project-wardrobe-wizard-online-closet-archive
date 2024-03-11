31e8ee2cef81e6dd5d71db1f9ac22ee5b9359372
ShiwenFang <sf3501@nyu.edu>
2024-03-11 12:34:02 -0400

    Each of the articles of clothing created and overlap window updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accessories from './screens/Accessories'; // This assumes Accessories.js is in the screens directory
+import './App.css'; // Your App's CSS
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Render the Accessories component as the main content of the App */}
+      <Accessories />
     </div>
   );
 }
---
+++ front-end/src/screens/Accessories.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../styles/Accessories.css'; // Make sure this path is correct
+import OverlayMenu from '../components/OverlayMenu'; // Import the OverlayMenu component
+
+const Accessories = () => {
+    // Mock data for Accessories
+    const Accessories = [
+        { name: 'Casual Accessories', brand: 'Brand A', type: 'Casual' },
+        { name: 'Formal Accessories', brand: 'Brand B', type: 'Formal' },
+        { name: 'Accessories 3', brand: 'Brand C', type: 'Fashion' },
+        { name: 'Accessories 4', brand: 'Brand D', type: 'New' },
+        // ... add more Accessories as needed
+    ];
+
+    return(
+        <div className="Accessories">
+            <OverlayMenu />
+            <header className='Accessories-banner'>
+                <h1>WARDROBE WIZARD</h1>
+                <h1>Accessories</h1>
+            </header>
+            <div className="Accessories-list">
+                {Accessories.map((Accessorie, index) => (
+                    <div className="Accessories-item" key={index}>
+                        <div className="Accessories-image"></div> {/* Placeholder for the image */}
+                        <div className="Accessories-info">
+                            <h3>{Accessorie.name}</h3>
+                            <p>{Accessorie.brand}</p>
+                            <p>{Accessorie.type}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Accessories;
