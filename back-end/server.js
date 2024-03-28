19a4f44029d150db5ae07fc200bbf2fbf0becc89
N-A-V-I-D <NC1939@nyu.edu>
2024-03-27 21:52:57 -0400

    created backend for login + registration and added nodemon, cors, axios

--- back-end/server.js
+++ back-end/server.js
@@ -2,9 +2,56 @@
 
 const server = require("./app") // load up the web server
 
-const port = 3000 // the port to listen to for incoming requests
+const port = 3001 // the port to listen to for incoming requests
 
 // call express's listen function to start listening to the port
+const accounts = [
+  {
+    "username": "tester",
+    "password": "tester"
+  },
+  {
+    "username": "example",
+    "password": "login"
+  }
+];
+
+server.post('/login', (req,res) => {
+  const loginSuccessful = {
+    'loggedIn': true
+  };
+  const loginUnsuccessful = {
+    'loggedIn': false
+  };
+  
+  console.log("login: ", req.body)
+  for(let i = 0; i < accounts.length; i++){
+    // console.log(req.body.username, req.body.password);
+    if(req.body.username == accounts[i].username){
+      if(req.body.password == accounts[i].password){
+        return res.json(loginSuccessful);
+      }
+    }
+  }
+  return res.json(loginUnsuccessful);
+
+})
+
+server.post('/register', (req,res) => {  
+  console.log("register:", req.body)
+  for(let i = 0; i < accounts.length; i++){
+    // console.log(req.body.username, req.body.password);
+    if(req.body.username == accounts[i].username){
+      return res.json({'message':'Username taken. Please choose a different one','created': false})
+    }
+  }
+  accounts.push(
+    { "username": req.body.username,
+      "password": req.body.password
+    })
+  return res.json({'message':'Account created','created': true})
+})
+
 const listener = server.listen(port, function () {
   console.log(`Server running on port: ${port}`)
 })
