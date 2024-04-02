5bed5a5a22afeae787cc66d03e4ebb462b20fd94
N-A-V-I-D <NC1939@nyu.edu>
2024-04-02 01:00:43 -0400

    added instructions to run

--- back-end/testServer_2.js
+++
@@ -1,9 +0,0 @@
-// import server from '../app.js';
-// import request from 'supertest';
-const server = require('./app.js');
-const request = require('supertest');
-
-test('example GET shirts', async () => {
-    const res = await request(server).get('/shirts');
-    expect(res.statusCode).toBe(200);
-})
