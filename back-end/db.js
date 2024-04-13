b4a87b389dec9539be53e8ca483c2b492c11168e
N-A-V-I-D <NC1939@nyu.edu>
2024-04-13 00:37:08 -0400

    wrote sample schema

--- back-end/db.js
+++ back-end/db.js
@@ -10,3 +10,21 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch((err) => {
         console.error('Error connecting to MongoDB', err);
     });
+
+const username_password = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {collection: 'user'});
+
+// const clothing_item = new mongoose.Schema({
+//     //id, name, brand, type, color, notes, imgLink
+// }, {collection: 'clothing'});
+
+
